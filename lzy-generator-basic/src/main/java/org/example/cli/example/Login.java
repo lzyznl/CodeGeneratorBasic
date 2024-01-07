74c3bbf5ca0e60b4376bb81e57f8a44924f850e6
lzyznl <2941649503@qq.com>
2024-01-07 11:41:09 +0800

    引入PicoCli命令行库，学习引入PicoCli

---
+++ lzy-generator-basic/src/main/java/org/example/cli/example/Login.java
@@ -0,0 +1,31 @@
+package org.example.cli.example;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+/**
+ * @author lzy
+ * @date 2024-01-07
+ * 实现交互输入以及可选式交互输入
+ */
+public class Login implements Callable<Integer> {
+    @Option(names = {"-u", "--user"}, description = "User name")
+    String user;
+
+    @Option(names = {"-p", "--password"}, arity = "0..1",description = "Passphrase", interactive = true)
+    String password;
+
+    @Option(names = {"-c", "--checkPassword"}, arity = "0..1",description = "check-password", interactive = true)
+    String checkPassword;
+
+    public Integer call() throws Exception {
+        System.out.println("password="+password);
+        System.out.println("checkPassword="+checkPassword);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        new CommandLine(new Login()).execute("--user","lzy","-p","123456","-c","123456");
+    }
+}
---
+++ lzy-generator-basic/src/main/java/org/example/cli/pattern/Command.java
@@ -0,0 +1,11 @@
+package org.example.cli.pattern;
+
+/**
+ * @author lzy
+ * @date 2024-01-07
+ * 命令接口
+ */
+public interface Command {
+
+    void execute();
+}
